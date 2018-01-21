b84c49a4961b5999d9587081206ce9bf412c7aa6
Humberto Barrientos <Hawksmoor@gmail.com>
2018-01-21 22:45:42 +0100

    adding todo to the server

--- src/actions.js
+++ src/actions.js
@@ -31,10 +31,11 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 
 
 
-export const addTodo = (text) => ({
-    type: "ADD_TODO",
-    text,
-    id: v4()
-})
-
+export const addTodo = (text) => (dispatch) =>
+  api.addTodo(text).then(response => {
+    dispatch({
+      type: 'ADD_TODO_SUCCESS',
+      response
+    })
+  })
 export const toggleTodo = (id) => ({ type: "TOGGLE_TODO", id })
